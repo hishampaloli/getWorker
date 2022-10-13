8ce9ddb26b8892ccce77662386a3350d95e6d180
Hisham Paloli <hishampaloli786@gmail.com>
2022-10-13 12:06:47 +0530

    scss-MyProposalResponsive

--- getwork.com/frontend/src/components/EmployeeComponents/ProposalComponents/ProposalComponent.js
+++ getwork.com/frontend/src/components/EmployeeComponents/ProposalComponents/ProposalComponent.js
@@ -5,6 +5,7 @@ import BookmarkRemoveIcon from "@mui/icons-material/BookmarkRemove";
 import { useDispatch } from "react-redux";
 import { removeSaveJobs, saveJobs } from "../../../actions/EmplyeeActions";
 import { Link } from "react-router-dom";
+import './ProposalComponent.scss'
 
 const ProposalComponent = ({ proposals, sort }) => {
   const dispatch = useDispatch();
@@ -30,22 +31,22 @@ const ProposalComponent = ({ proposals, sort }) => {
       {proposals
         ? proposals?.map((proposal) => {
             return (
-              <div className="emp-jobs-div">
+              <div className="emp-prop-div">
                 <div className="s">
                   <h4>{proposal?.cover?.slice(0, 20)}...</h4>
 
-                  <div style={{display: 'flex', alignItems: 'center', width: '100%'}} className="rowEm ">
+                  <div  className="rowEm ">
                     <p className="mt-2">
                      Your Bid: <strong>${proposal?.bid}</strong>
                     </p>
-                    <p style={{whiteSpace: 'nowrap', width: '200px'}} className="rpEm mt-2">
+                    <p style={{whiteSpace: 'nowrap'}} className="rpEm mt-2">
                       Deadline: <strong>{proposal?.deadline} days</strong>
                     </p>
                   </div>
                 </div>
 
                 <Link to={`/user/proposal/${proposal?._id}`}>
-                  <button style={{ marginLeft: "auto" }} className="eyebtnnn">
+                  <button style={{ marginLeft: "auto" }} className="eyebtnnx">
                     <VisibilityIcon />
                   </button>
                 </Link>
